a85c6aae1b0a63d8bdc302091ef11a15fb49d7a8
Sergey Boishtyan <vitilus123@gmail.com>
2021-08-25 22:02:32 +0300

    LoginViewModel to MVVM

--- app/src/main/java/ru/awac/mvvm_homework/ui/login/LoginViewModel.kt
+++ app/src/main/java/ru/awac/mvvm_homework/ui/login/LoginViewModel.kt
@@ -5,8 +5,18 @@ import ru.awac.mvvm_homework.data.User
 import ru.awac.mvvm_homework.data.UserRepository
 
 class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
+    
+    private val user: LiveData<User> = TODO()
+    
+    val showMessages: LiveData<Boolean> = TODO()
+    val login: LiveData<String> = TODO()
+    val password: LiveData<String> = TODO()
 
     fun getAllUsers() = userRepository.getAllUsers()
 
     fun addUser(user: User) = userRepository.addUser(user)
+    
+    fun onRegisterClick() {
+        showMessages.set(false)            
+    }
 }
